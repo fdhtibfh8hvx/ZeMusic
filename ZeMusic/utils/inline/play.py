ca3f0d9cd1f6193db09c10a992714521aab08e12
yousef1yt <167021222+yousef1yt@users.noreply.github.com>
2024-05-07 02:49:14 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -56,14 +56,14 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————◉"
     buttons = [
                 [
-            InlineKeyboardButton(text="𖣂 𝒓𝒆𝒔𝒖𝒎𝒆 𖣂", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 𝒑𝒂𝒖𝒔𝒆 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 𝒓𝒆𝒑𝒍𝒂𝒚 𖣂", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 استمرار 𖣂", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 ايقاف 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 اعاده 𖣂", callback_data=f"ADMIN Replay|{chat_id}"),
         ],[
-            InlineKeyboardButton(text="𖣂 𝒔𝒌𝒊𝒑 𖣂", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 𝒔𝒕𝒐𝒑 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 تخطي 𖣂", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 ايقاف 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text="𐇮 𝑴𝑶𝑫𝒀 𖠮🚸𖠮 آلـۘهہؚيـٰـ‌ُـُ໋۠بـ໋ۘ۠ه 𐇮", url=f"https://t.me/D_8_N"),
+            InlineKeyboardButton(text="⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"),
         ],[
             InlineKeyboardButton(text="𖣂 اضف البوت الي مجموعتك او قناتك 𖣂", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
@@ -73,14 +73,14 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="𖣂 𝒓𝒆𝒔𝒖𝒎𝒆 𖣂", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 𝒑𝒂𝒖𝒔𝒆 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 𝒓𝒆𝒑𝒍𝒂𝒚 𖣂", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 استمرار 𖣂", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 ايقاف 𖣂", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 اعاده 𖣂", callback_data=f"ADMIN Replay|{chat_id}"),
         ],[
-            InlineKeyboardButton(text="𖣂 𝒔𝒌𝒊𝒑 𖣂", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="𖣂 𝒔𝒕𝒐𝒑 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 تخطي 𖣂", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="𖣂 ايقاف 𖣂", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text="𐇮 𝑴𝑶𝑫𝒀 𖠮🚸𖠮 آلـۘهہؚيـٰـ‌ُـُ໋۠بـ໋ۘ۠ه 𐇮", url=f"https://t.me/D_8_N"),
+            InlineKeyboardButton(text="⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"),
         ],[
             InlineKeyboardButton(text="𖣂 اضف البوت الي مجموعتك او قناتك 𖣂", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
